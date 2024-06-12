c8f211bd6de1edb47f9cebc9c87cc6d1de12732b
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-06-12 09:01:39 +0700

    tao project

---
+++ src/main/java/com/example/bicyle/BicyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bicyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bicyle/BicyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bicyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
