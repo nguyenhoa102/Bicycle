04e7a12d243cd328452f5b7cae111ca3490fc32e
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-06-12 11:13:14 +0700

    a

--- src/main/java/com/example/bicyle/BicycleApplication.java
+++ src/main/java/com/example/bicyle/BicycleApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BicyleApplication {
+public class BicycleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BicyleApplication.class, args);
+		SpringApplication.run(BicycleApplication.class, args);
 	}
-
 }
